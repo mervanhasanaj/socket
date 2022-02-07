02d9dcdcced6559e4ab07aff60af6b3e34edaf81
mervanhasanaj <63062971+mervanhasanaj@users.noreply.github.com>
2022-02-07 20:52:12 +0100

    esercizi pagelle

--- pagelle/pagelle_client.py
+++ pagelle/pagelle_client.py
@@ -1,4 +1,3 @@
-from base64 import decode
 import socket
 import json
 
@@ -6,23 +5,28 @@ HOST="127.0.0.1"
 PORT=65432
 
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-    #af inet indica il tipo di protocollo che viene usato
-    #socket stream indica che tipo di socket è
-    #whith as tiene aperto i socket dandogli come nome s,
-    #dopo che il codice è finito chiuderà la connessione con il server
-    s.connect((HOST,PORT))
+    #AF_INEF: tipo di protocollo #STREAM: Indica la connessione
+    
+    s.connect((HOST, PORT))
     while True:
-        frase=input("inserisci la frase ")
-        if frase=="KO":
-            print('Sessione terminata')
+        operazione=input("Comandi disponibili: \n #list : vedere i voti inseriti \n #set /nomestudente : per inserire uno studente \n #put /nomestudente/materia/voto/ore \n #get /nomestudente : per richiedere i voti di uno studente \n #close: per chiudere la connessione con il server e spegnerlo\n")
+        messaggio={
+            'operazione' : operazione
+        }
+        messaggio=json.dumps(messaggio)
+        s.sendall(messaggio.encode("UTF-8"))
+        data=s.recv(1024)
+        data=data.decode()
+        print(data)
+        if operazione=='#list':
+            deserialized_dict=json.loads(data)
+            #viene effettuata la decodifica
+            print(deserialized_dict)
+        elif operazione.find('#set') != -1:
+            print(data)
+        elif operazione=='#close':
+            print("Connessione chuisa")
             break
-        frase=str(frase)
-        messaggio={'frase':frase}
-
-        messaggio=json.dumps(messaggio)# trasformiamo l'oggetto in una stringa
-        s.sendall(messaggio.encode("UTF-8"))# invia il vettore di byte
-
-        data=s.recv(1024)# aspettiamo che il server ci rimandi in dietro i dati 
-        print("Risultato: ",data,decode)# trasforma il vettore di byte in stringa
-        
-
+        elif operazione.find('#put') != -1:
+            print(data)
+        print("\n")
--- pagelle/pagelle_server.py
+++ pagelle/pagelle_server.py
@@ -1,45 +1,67 @@
-from http import client
+from operator import truediv
 import socket
 import json
 
-HOST="127.0.0.1"
+HOST='127.0.0.1'
 PORT=65432
 
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-    progressivo=1
-    s.bind((HOST,PORT))
+    s.bind((HOST, PORT))
     s.listen()
-    print("[*] In ascolto su %s:%d"%(HOST,PORT))
-    clientsocket, address=s.accept()
-
-with clientsocket as cs:
-    print("Conessione da ",address)
-    while True:
-        data=cs.recv(1024)
-        if not data:
-            break
-        data=data.decode()
-        data=json.loads(data)
-        frase=data['frase']
-        ris=""
-        if type(frase)==str:
-            ris=str(progressivo) + ') '+ frase
-            progressivo+=1
-        else:
-            ris="Operazione non riconosciuta"
-        '''
-        elif operazione=="*":
-            ris=primoNumero*secondoNumero
-        elif operazione=="/":
-            if secondoNumero==0:
-                ris="non puoi dividere per 0"
-            else:
-                ris=primoNumero/secondoNumero
-        elif operazione=="%":
-            ris=primoNumero%secondoNumero
-        else:
-            ris="Operazione non riconosciuta"
-        '''
-        ris=str(ris)#casting
-        print('Ricevuto: ' + ris)
-        cs.sendall(ris.encode("UTF-()"))
+    print("[*] In ascolto su %s:%d "%(HOST, PORT))
+    clientsocket, address=s.accept()#accetta la conversione
+    students = { 
+                'Giuseppe Gullo':[("Matematica",9,0),("Italiano",7,3),("Inglese",7.5,4),("Storia",7.5,4),("Geografia",5,7)],
+                'Antonio Barbera':[("Matematica",8,1),("Italiano",6,1),("Inglese",9.5,0),("Storia",8,2),("Geografia",8,1)],
+                'Nicola Spina':[("Matematica",7.5,2),("Italiano",6,2),("Inglese",4,3),("Storia",8.5,2),("Geografia",8,2)]
+                }
+    with clientsocket as cs:
+        print("Connessione da ", address)
+        while True:
+            data=cs.recv(1024)
+            if not data:
+                break
+            data=data.decode()
+            data=data.strip()
+            data=json.loads(data)
+            comando=data['operazione']
+            print ("[*] Recived: %s" % comando)
+            if comando == '#list':
+                serialized_stud = json.dumps(students)
+                cs.sendall(serialized_stud.encode("UTF-8"))
+            elif comando.find('#set') != -1:
+                inserimento=comando.split('/')
+                controllo_pres=inserimento[1]
+                if(controllo_pres in students):
+                    cs.sendall("Studente già presente".encode("UTF-8"))
+                else:
+                    students[inserimento[1]]=[]
+                    cs.sendall("Studente inserito".encode("UTF-8"))
+            elif comando.find('#get') != -1:
+                inserimento=comando.split('/')
+                controllo_pres=inserimento[1]
+                if(controllo_pres in students):
+                    serialized_stud = json.dumps(students[controllo_pres])
+                    cs.sendall(serialized_stud.encode("UTF-8"))
+                else:
+                    cs.sendall("Studente non presente".encode("UTF-8"))
+            elif comando.find('#put') != -1:
+                boole=True
+                inserimento=comando.split('/')
+                controllo_pres=inserimento[1]
+                controllo_materia=inserimento[2]
+                if(controllo_pres in students):
+                    for alunno,materie in students.items():
+                        if(alunno==controllo_pres):
+                            for i in materie:
+                                if(controllo_materia == i[0]):
+                                    cs.sendall("Materia già presente".encode("UTF-8"))
+                                    boole=False
+                            if(boole==True):    
+                                students[inserimento[1]].append((inserimento[2],inserimento[3],inserimento[4]))
+                                cs.sendall("Inserimento avvenuto".encode("UTF-8"))
+                else:
+                    cs.sendall("Studente non trovato".encode("UTF-8"))
+            elif comando.find('#close') != -1:
+                break
+            print(cs.getpeername())
