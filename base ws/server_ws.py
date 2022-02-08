cf19bc90ed7a0232c9554841b4025f6127cebed0
mervanhasanaj <63062971+mervanhasanaj@users.noreply.github.com>
2022-02-08 10:21:43 +0100

    nuovo es

---
+++ base ws/server_ws.py
@@ -0,0 +1,42 @@
+from http import client
+import socket
+import json
+
+HOST="127.0.0.1"
+PORT=22012
+
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+    s.bind((HOST,PORT))
+    s.listen()
+    print("[*] In ascolto su %s:%d"%(HOST,PORT))
+    clientsocket, address=s.accept()
+
+with clientsocket as cs:
+    print("Conessione da ",address)
+    while True:
+        data=cs.recv(1024)
+        if not data:
+            break
+        data=data.decode()
+        data=json.loads(data)
+        primoNumero=data['primoNumero']
+        operazione=data['operazione']
+        secondoNumero=data['secondoNumero']
+        ris=""
+        if operazione=="+":
+            ris=primoNumero+secondoNumero
+        elif operazione=="-":
+            ris=primoNumero-secondoNumero
+        elif operazione=="*":
+            ris=primoNumero*secondoNumero
+        elif operazione=="/":
+            if secondoNumero==0:
+                ris="non puoi dividere per 0"
+            else:
+                ris=primoNumero/secondoNumero
+        elif operazione=="%":
+            ris=primoNumero%secondoNumero
+        else:
+            ris="Operazione non riconosciuta"
+        ris=str(ris)#casting 
+        cs.sendall(ris.encode("UTF-()"))
