30f66ecc7031da8232f6a1ae6974ad708239c7e4
mervanhasanaj <63062971+mervanhasanaj@users.noreply.github.com>
2022-02-10 11:40:43 +0100

    commenti

--- notebook/notebook1.2.py
+++ notebook/notebook1.2.py
@@ -1,17 +1,26 @@
 import time
 import logging
 from threading import Thread
+
+
 format = "%(asctime)s: %(message)s"
 logging.basicConfig(format=format, level=logging.INFO,
                     datefmt="%H:%M:%S")
 
+#creiamo la funzione test e passiamo i come parametro
 def test(i):
+  #informa che il thread è iniziato
   logging.info(f"{i} thread: iniziato")
+  #ferma la funzione su 3 secondi
   time.sleep(3)
+  #informa che il thread è finito
   logging.info(f"{i} thread: finito")
 
 
+#ciclo for per creare 5 thread
 for i in range(5):
+  #vreazione del tread
   t = Thread(target=test, args=(i,))
+  #inizio
   t.start()
   t.join()
--- notebook/notebook1.py
+++ notebook/notebook1.py
@@ -1,22 +1,41 @@
 from threading import Thread
 import time, datetime
+
+#creazione funzione thread1
 def threadl():
-  print ("Thread 1 iniziato")
-  time.sleep(10)
-  print ("Thread 1 finito")
+    #avviso che è iniziato il primo thread
+    print ("Thread 1 iniziato")
+    #fermiamo il thread per 10 secondi
+    time.sleep(10)
+    #diciamo che il thread è finito
+    print ("Thread 1 finito")
+
+#creazione funzione thread2
 def thread2():
-  print ("Thread 2 iniziato")
-  time.sleep(4)
-  print ("Thread 2 finito")
-  
+    #avviso che è iniziato il secondo thread
+    print ("Thread 2 iniziato")
+    #fermiamo il thread per 4 secondi
+    time.sleep(4)
+    #diciamo che il thread è finito
+    print ("Thread 2 finito")
+
+#avviso di inizio codice
 print ("Main iniziato")
 
+#il tempo di inizio è = all'ora corrente
 start_time = time.time()
+#creazione thread
 t1 = Thread(target=threadl)
+#creazione thread
 t2 = Thread(target=thread2)
+#thread 1 parte
 t1.start()
+#thread 2 parte
 t2. start()
+#fermiamo il tempo per 2 secondi
 time.sleep(2)
+#tempo di fine è = al tempo corrente
 end_time = time.time()
 
+#mostraiamo il il tempo di fine e di inizio
 print (f"Main finito in {end_time-start_time}")
